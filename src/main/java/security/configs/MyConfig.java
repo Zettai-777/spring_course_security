3026e2e95e3b9887177bf4cbc2d3573165c0b003
Zettai-777 <zettai777@gmail.com>
2021-02-15 21:12:35 +0300

    Simple Spring MVC + Spring Security application. Added supporting of databases for storing logins|passwords(noop in this moment) instead in memory style. Created necessary tables.

--- src/main/java/security/configs/MyConfig.java
+++ src/main/java/security/configs/MyConfig.java
@@ -1,11 +1,15 @@
 package security.configs;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 @ComponentScan(basePackages = "security")
@@ -19,6 +23,20 @@ public class MyConfig {
         resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".jsp");
         return resolver;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/db_hibernate_test?useSSL=false&serverTimezone=UTC");
+            dataSource.setUser("zettai");
+            dataSource.setPassword("zettai");
+        }catch (PropertyVetoException e){
+            System.out.println(e.getMessage());
+        }
+        return dataSource;
     }
 
 }
--- src/main/java/security/configs/MySecurityConfig.java
+++ src/main/java/security/configs/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package security.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,27 +8,43 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
-    //Аутентификация (1 уровень защиты)
+    @Autowired
+    DataSource dataSource;
+
+//    Аутентификация (1 уровень защиты) через in memory data
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication().
+//                withUser(userBuilder.username("Michail")
+//                        .password("Michail")
+//                        .roles("EMPLOYEE"))
+//                .withUser(userBuilder.username("Anastasia")
+//                        .password("Anastasia")
+//                        .roles("HR"))
+//                .withUser(userBuilder.username("Ivan")
+//                        .password("Ivan")
+//                        .roles("MANAGER", "HR"));
+//    }
+
+
+//  Аутентификация (1 уровень защиты) через БД
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication().
-                withUser(userBuilder.username("Michail")
-                        .password("Michail")
-                        .roles("EMPLOYEE"))
-                .withUser(userBuilder.username("Anastasia")
-                        .password("Anastasia")
-                        .roles("HR"))
-                .withUser(userBuilder.username("Ivan")
-                        .password("Ivan")
-                        .roles("MANAGER", "HR"));
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.a
+//    }
 
-    //Авторизация (2 уровень защиты)
+    //Авторизация (2 уровень защиты) через in memory data
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
